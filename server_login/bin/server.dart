aeac20f470331097f0017796f4a473caaad223ed
Zengcheng283 <1265207567@qq.com>
2022-11-01 08:27:33 +0000

    add login_dart

---
+++ server_login/bin/server.dart
@@ -0,0 +1,5 @@
+import 'package:server_login/server_login.dart' as server_login;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_login.calculate()}!');
+}
---
+++ server_login/test/server_login_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_login/server_login.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
